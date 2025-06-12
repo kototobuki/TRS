36152447d3cb9ac635340e53ef52b1b68f10b078
DESKTOP-USQB64V\sen86 <sen.86.og@iCloud.com>
2025-06-13 01:11:58 +0900

    レビューをいただいた箇所修正

---
+++ script.js
@@ -0,0 +1,12 @@
+  function showForm(type) {
+    const personalForm = document.getElementById('form-personal');
+    const corporateForm = document.getElementById('form-corporate');
+
+    if (type === 'personal') {
+      personalForm.style.display = 'flex';
+      corporateForm.style.display = 'none';
+    } else if (type === 'corporate') {
+      personalForm.style.display = 'none';
+      corporateForm.style.display = 'flex';
+    }
+  }

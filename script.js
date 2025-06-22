26bc6e6825af40d87e4b4d63987abe21d16a9fd6
DESKTOP-USQB64V\sen86 <sen.86.og@iCloud.com>
2025-06-23 08:35:42 +0900

    contact修正 アニメーション追加

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// メニュー表示
 const openBtn = document.querySelector(".open-btn");
 const menu = document.querySelector(".menu");
 const closeBtn = document.querySelector(".close-btn span");
@@ -22,4 +23,65 @@ closeBtn.addEventListener("click", () => {
     menu.style.display = "none";
     menu.classList.remove("hide"); 
   }, 500); 
+});
+
+// フォームの切り替え
+$(function () {
+  $("#btn-personal").on("click", function () {
+    $("#form-corporate").hide();
+    $("#form-personal").show();
+  });
+
+  $("#btn-corporate").on("click", function () {
+    $("#form-personal").hide();
+    $("#form-corporate").show();
+  });
+});
+
+//　スライド
+$(function () {
+  $("#btn-personal").on("click", function () {
+    $("#form-corporate").hide();
+    $("#form-personal").show();
+
+    $("html, body").animate({
+      scrollTop: $("#form-personal").offset().top
+    }, 600);
+  });
+
+  $("#btn-corporate").on("click", function () {
+    $("#form-personal").hide();
+    $("#form-corporate").show();
+
+    $("html, body").animate({
+      scrollTop: $("#form-corporate").offset().top
+    }, 600);
+  });
+});
+// 高さ制御
+document.getElementById("btn-personal").addEventListener("click", () => {
+  const form = document.getElementById("form-personal");
+  form.style.display = "flex";
+  document.getElementById("form-corporate").style.display = "none";
+
+  // スクロール位置を調整
+  const formRect = form.getBoundingClientRect();
+  const offset = formRect.bottom - window.innerHeight;
+  window.scrollBy({
+    top: offset,
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("btn-corporate").addEventListener("click", () => {
+  const form = document.getElementById("form-corporate");
+  form.style.display = "flex";
+  document.getElementById("form-personal").style.display = "none";
+
+  const formRect = form.getBoundingClientRect();
+  const offset = formRect.bottom - window.innerHeight;
+  window.scrollBy({
+    top: offset,
+    behavior: "smooth"
+  });
 });
